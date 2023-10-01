6dce530256dccc2bbd0046d15dbb10df64805559
You-Kenhei <you.kenhei.yjp@outlook.com>
2023-10-02 01:33:39 +0900

    1

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,22 @@
 <template>
-  <div></div>
+  <div>
+    <!-- <h1>{{ message }}</h1> -->
+    <element-view></element-view>
+  </div>
 </template>
 
 <script>
+import ElementView from './views/element/ElementView.vue';
 export default {
-  
+  components: { ElementView },
+  data () {
+    return {
+      message:"Hello Vue"
+    }
+  },
+  methods: {
+
+  }
 };
 </script>
 
