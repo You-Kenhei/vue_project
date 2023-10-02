af3f9a4736bb974fe41cb43e98a925570a194f07
You-Kenhei <you.kenhei.yjp@outlook.com>
2023-10-02 23:05:40 +0900

    1

--- src/views/tlias/EmpView.vue
+++ src/views/tlias/EmpView.vue
@@ -1,14 +1,16 @@
 <template>
   <div>
     <el-container style="height: 700px; border: 1px solid #eee">
-
-      <el-header style="font-size: 40px; background-color: rgb(238, 241, 246)">tlias 智能学习辅助系统</el-header>
+      <el-header
+                 style="font-size: 40px; background-color: rgb(238, 241, 246)">tlias
+        智能学习辅助系统</el-header>
 
       <el-container>
         <el-aside width="200px">
           <el-menu :default-openeds="['1', '3']">
             <el-submenu index="1">
-              <template slot="title"><i class="el-icon-message"> </i>系统信息管理</template>
+              <template slot="title"><i class="el-icon-message">
+                </i>系统信息管理</template>
               <el-menu-item index="1-1">部门管理</el-menu-item>
               <el-menu-item index="1-2">员工管理</el-menu-item>
             </el-submenu>
@@ -17,37 +19,69 @@
 
         <el-main>
           <!-- Form表单 -->
-          <el-form :inline="true" :model="searchForm" class="demo-form-inline">
+          <el-form :inline="true"
+                   :model="searchForm"
+                   class="demo-form-inline">
             <el-form-item label="姓名">
-              <el-input v-model="searchForm.name" placeholder="姓名"></el-input>
+              <el-input v-model="searchForm.name"
+                        placeholder="姓名"></el-input>
             </el-form-item>
             <el-form-item label="性别">
-              <el-select v-model="searchForm.gender" placeholder="性别">
-                <el-option label="男" value="1"></el-option>
-                <el-option label="女" value="2"></el-option>
+              <el-select v-model="searchForm.gender"
+                         placeholder="性别">
+                <el-option label="男"
+                           value="1"></el-option>
+                <el-option label="女"
+                           value="2"></el-option>
               </el-select>
             </el-form-item>
             <el-form-item label="入职日期">
-              <el-date-picker v-model="searchForm.entrydate" type="daterange" range-separator="至" start-placeholder="开始日期"
-                end-placeholder="结束日期">
+              <el-date-picker v-model="searchForm.entrydate"
+                              type="daterange"
+                              range-separator="至"
+                              start-placeholder="开始日期"
+                              end-placeholder="结束日期">
               </el-date-picker>
             </el-form-item>
             <el-form-item>
-              <el-button type="primary" @click="onSubmit">查询</el-button>
+              <el-button type="primary"
+                         @click="onSubmit">查询</el-button>
             </el-form-item>
           </el-form>
-          <el-table :data="tableData">
-            <el-table-column prop="date" label="姓名" width="180"></el-table-column>
-            <el-table-column prop="name" label="图像" width="180"></el-table-column>
-            <el-table-column prop="gender" label="性别" width="140"></el-table-column>
-            <el-table-column prop="job" label="职位" width="140"></el-table-column>
-            <el-table-column prop="entrydate" label="入职日期" width="180"></el-table-column>
-            <el-table-column prop="updatetime" label="最后操作时间" width="230"></el-table-column>
+          <el-table :data="tableData"
+                    border>
+            <el-table-column prop="date"
+                             label="姓名"
+                             width="180"></el-table-column>
+            <el-table-column prop="name"
+                             label="图像"
+                             width="180"></el-table-column>
+            <el-table-column prop="gender"
+                             label="性别"
+                             width="140"></el-table-column>
+            <el-table-column prop="job"
+                             label="职位"
+                             width="140"></el-table-column>
+            <el-table-column prop="entrydate"
+                             label="入职日期"
+                             width="180"></el-table-column>
+            <el-table-column prop="updatetime"
+                             label="最后操作时间"
+                             width="230"></el-table-column>
             <el-table-column label="操作">
-              <el-button type="primary" size="mini">编辑</el-button>
-              <el-button type="danger" size="mini">删除</el-button>
+              <el-button type="primary"
+                         size="mini">编辑</el-button>
+              <el-button type="danger"
+                         size="mini">删除</el-button>
             </el-table-column>
           </el-table>
+          <!-- pagination分页 -->
+          <el-pagination background
+                         layout="sizes, prev, pager, next, jumper, ->, total, slot"
+                         :total="1000"
+                         @size-change="handleSizeChange"
+                         @current-change="handleCurrentChange">
+          </el-pagination>
         </el-main>
       </el-container>
     </el-container>
@@ -62,14 +96,20 @@ export default {
       searchForm: {
         name: "",
         gender: "",
-        entrydate: []
-      }
-    }
+        entrydate: [],
+      },
+    };
   },
   methods: {
     onSubmit() {
-      alert(JSON.stringify(this.searchForm))
-    }
+      alert(JSON.stringify(this.searchForm));
+    },
+    handleSizeChange(val) {
+      alert(val);
+    },
+    handleCurrentChange(val) {
+      alert(val);
+    },
   },
 };
 </script>
