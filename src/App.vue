65d5b7b9ae66bfdb68b5a6f8c83dc57e8bb940a8
You-Kenhei <you.kenhei.yjp@outlook.com>
2023-10-02 22:11:54 +0900

    1

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,18 @@
 <template>
   <div>
     <!-- <h1>{{ message }}</h1> -->
-    <element-view></element-view>
+    <!-- <element-view></element-view> -->
+
+    <emp-view></emp-view>
   </div>
 </template>
 
 <script>
-import ElementView from './views/element/ElementView.vue';
+import EmpView from './views/tlias/EmpView.vue';
+// import ElementView from './views/element/ElementView.vue';
+
 export default {
-  components: { ElementView },
+  components: { /* ElementView, */EmpView },
   data () {
     return {
       message:"Hello Vue"
---
+++ src/views/tlias/EmpView.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <el-container style="height: 700px; border: 1px solid #eee">
+      <el-header style="font-size: 40px; background-color: rgb(238, 241, 246)">tlias 智能学习辅助系统</el-header>
+      <el-container>
+        <el-aside width="200px"><el-menu :default-openeds="['1', '3']">
+            <el-submenu index="1">
+              <template slot="title"><i class="el-icon-message"> </i>系统信息管理</template>
+
+              <el-menu-item index="1-1">部门管理</el-menu-item>
+              <el-menu-item index="1-2">员工管理</el-menu-item>
+            </el-submenu>
+          </el-menu>
+        </el-aside>
+        <el-main>
+          <el-table :data="tableData">
+            <el-table-column prop="date" label="姓名" width="180"></el-table-column>
+            <el-table-column prop="name" label="图像" width="180"></el-table-column>
+            <el-table-column prop="gender" label="性别" width="140"></el-table-column>
+            <el-table-column prop="job" label="职位" width="140"></el-table-column>
+            <el-table-column prop="entrydate" label="入职日期" width="180"></el-table-column>
+            <el-table-column prop="updatetime" label="最后操作时间" width="230"></el-table-column>
+            <el-table-column label="操作">
+              <el-button type="primary" size="mini">编辑</el-button>
+              <el-button type="danger" size="mini">删除</el-button>
+            </el-table-column>
+          </el-table>
+        </el-main>
+      </el-container>
+    </el-container>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      tableData: []
+    }
+  }
+};
+</script>
+
+<style></style>
