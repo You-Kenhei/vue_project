bcafb12f9fd0f23d6bb336c46342635352b0336c
You-Kenhei <you.kenhei.yjp@outlook.com>
2023-10-02 16:21:15 +0900

    Update ElementView.vue

--- src/views/element/ElementView.vue
+++ src/views/element/ElementView.vue
@@ -26,6 +26,72 @@
       @current-change="handleCurrentChange"
     >
     </el-pagination>
+
+    <br /><br />
+
+    <!-- Dialog对话框组件 内嵌table -->
+    <el-button type="text" @click="dialogTableVisible = true"
+      >打开嵌套Table的 Dialog</el-button
+    >
+
+    <el-dialog title="收货地址" :visible.sync="dialogTableVisible">
+      <el-table :data="gridData">
+        <el-table-column
+          property="date"
+          label="日期"
+          width="150"
+        ></el-table-column>
+        <el-table-column
+          property="name"
+          label="姓名"
+          width="200"
+        ></el-table-column>
+        <el-table-column property="address" label="地址"></el-table-column>
+      </el-table>
+    </el-dialog>
+
+    <br /><br />
+
+    <!-- Dialog对话框组件 内嵌Form表单 -->
+    <el-button type="text" @click="dialogFormVisible = true"
+      >打开嵌套Form的 Dialog</el-button
+    >
+
+    <el-dialog title="From表单" :visible.sync="dialogFormVisible"
+      ><el-form ref="form" :model="form" label-width="80px">
+        <el-form-item label="活动名称">
+          <el-input v-model="form.name"></el-input>
+        </el-form-item>
+        <el-form-item label="活动区域">
+          <el-select v-model="form.region" placeholder="请选择活动区域">
+            <el-option label="区域一" value="shanghai"></el-option>
+            <el-option label="区域二" value="beijing"></el-option>
+          </el-select>
+        </el-form-item>
+        <el-form-item label="活动时间">
+          <el-col :span="11">
+            <el-date-picker
+              type="date"
+              placeholder="选择日期"
+              v-model="form.date1"
+              style="width: 100%"
+            ></el-date-picker>
+          </el-col>
+          <el-col class="line" :span="2">-</el-col>
+          <el-col :span="11">
+            <el-time-picker
+              placeholder="选择时间"
+              v-model="form.date2"
+              style="width: 100%"
+            ></el-time-picker>
+          </el-col>
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary" @click="onSubmit">提交</el-button>
+          <el-button @click="dialogFormVisible = false">取消</el-button>
+        </el-form-item>
+      </el-form>
+    </el-dialog>
   </div>
 </template>
 
@@ -33,6 +99,36 @@
 export default {
   data() {
     return {
+      gridData: [
+        {
+          date: "2016-05-02",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1518 弄"
+        },
+        {
+          date: "2016-05-04",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1518 弄"
+        },
+        {
+          date: "2016-05-01",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1518 弄"
+        },
+        {
+          date: "2016-05-03",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1518 弄"
+        }
+      ],
+      dialogTableVisible: false,
+      dialogFormVisible: false,
+      form: {
+        name: "",
+        region: "",
+        date1: "",
+        date2:""
+      },
       tableData: [
         {
           date: "2016-05-02",
@@ -55,17 +151,20 @@ export default {
           address: "上海市普陀区金沙江路 1516 弄"
         }
       ]
-    }
+    };
   },
   methods: {
     handleSizeChange(val) {
-      alert("每页记录数变化" + val)
+      alert("每页记录数变化" + val);
     },
     handleCurrentChange(val) {
-      alert("页码发生变化" + val)
+      alert("页码发生变化" + val);
+    },
+    onSubmit() {
+        console.log(JSON.stringify(this.form));
     }
   }
-}
+};
 </script>
 
 <style>
